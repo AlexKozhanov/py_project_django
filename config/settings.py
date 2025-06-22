def448c9e48c0c3382446236df8ff7704b5d8ba6
Admin <alexander_kozhanov@mail.ru>
2025-06-22 23:53:05 +0300

    commit

--- config/settings.py
+++ config/settings.py
@@ -1,13 +1,17 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# override=True - явно перезаписывай переменные окружения, если они уже объявлены
+load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-%zrbokc$1d%#)09rxzy$38u1_w32)dyg17q==!$x#cp5l)6+re'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True # cntr+C после изменений и сервер сам перезапустится
+DEBUG = True if os.getenv('DEBUG') == "True" else False
 
 ALLOWED_HOSTS = []
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -49,11 +53,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'host': os.getenv('host'),
+        'port': os.getenv('port')
     }
 }
 
@@ -73,7 +80,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -81,8 +87,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
